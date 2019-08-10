55e8cbf2034210fc643f49c3a36fa0555cb879e8
Sean McPherson <mail@seanmcp.com>
2019-08-09 22:27:26 -0400

    Add form to add neighbor

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from 'react'
 import './App.css'
+import useLocalStorage from './useLocalStorage'
 
 function App() {
-    return <div className="App">pwc</div>
+    const [state, , { addNeighbor, findTodaysBirthdays }] = useLocalStorage()
+    function handleSubmit(event) {
+        const form = event.target
+        event.preventDefault()
+        const formData = new FormData(form)
+        const name = formData.get('name')
+        if (name) {
+            addNeighbor(name)
+            form.reset()
+        }
+    }
+    return (
+        <div className="App">
+            <code>{JSON.stringify(state, null, 2)}</code>pwc
+            <button onClick={() => addNeighbor('Sean')}>Add</button>
+            <code>{JSON.stringify(findTodaysBirthdays(), null, 2)}</code>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="neighbor-input">Neighbor</label>
+                <input id="neighbor-input" name="name" type="text" />
+                <button>Add</button>
+            </form>
+        </div>
+    )
 }
 
 export default App
