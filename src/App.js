3d53a124bb3c6eb1bf7dff8f7333a72aa8779197
Sean McPherson <mail@seanmcp.com>
2019-08-13 20:53:56 -0400

    Update app, router

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
 import React from 'react'
 import './App.css'
-import useLocalStorage from './useLocalStorage'
-import Recommendations from './Recommendations'
-import NeighborsList from './NeighborsList'
+import { LocalStorageProvider } from './useLocalStorage'
+import Router from './Router'
 
 function App() {
-    const [state, { addNeighbor, getPrayerRecommendations }] = useLocalStorage()
-    function handleSubmit(event) {
-        event.preventDefault()
-        const form = event.target
-        const formData = new FormData(form)
-        const name = formData.get('name')
-        if (name) {
-            addNeighbor(name)
-            form.reset()
-        }
-    }
     return (
         <div className="App">
-            <NeighborsList neighbors={state} />
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="neighbor-input">Neighbor</label>
-                <input id="neighbor-input" name="name" type="text" />
-                <button>Add</button>
-            </form>
-            <Recommendations
-                neighbors={state}
-                {...getPrayerRecommendations()}
-            />
+            <LocalStorageProvider>
+                <Router />
+            </LocalStorageProvider>
         </div>
     )
 }
--- src/Router.js
+++ src/Router.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import { Router } from '@reach/router'
 import AddNeighborView from './AddNeighborView'
+import NeighborView from './NeighborView'
 import NeighborsView from './NeighborsView'
 
 const Home = () => <div>Home</div>
-const ViewNeighbor = () => <div>Neighbor</div>
 
 export default function() {
     return (
         <Router>
             <Home path="/" />
             <AddNeighborView path="/neighbor/add" />
-            <ViewNeighbor path="/neighbor/:id" />
+            <NeighborView path="/neighbor/:id" />
             <NeighborsView path="/neighbors" />
         </Router>
     )
