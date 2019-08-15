ac789f8d74b0e40ab927bb411d46a1df92b7dea8
Sean McPherson <mail@seanmcp.com>
2019-08-15 06:33:13 -0400

    Add individual info

--- src/views/IndividualView.js
+++ src/views/IndividualView.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { navigate } from '@reach/router'
 import ROUTES from 'constants/routes'
 import { useIndividuals } from 'store/useIndividuals'
+import Emoji from 'a11y-react-emoji'
 
 function IndividualView(props) {
     const [, { get, remove }] = useIndividuals()
@@ -10,8 +11,29 @@ function IndividualView(props) {
         remove(props.id)
         navigate(ROUTES.all)
     }
+    function formatBirthday() {
+        const date = new Date(data.birthday)
+        return date.getMonth() + '/' + date.getDate()
+    }
+    function formatLastPrayed() {
+        const date = new Date(data.lastPrayed)
+        return date.getMonth() + '/' + date.getDate() + '/' + date.getFullYear()
+    }
     return (
         <div className="IndividualView">
+            <header>
+                <h1>{data.name}</h1>
+                <section>
+                    <div>
+                        <Emoji symbol="🙏" />
+                        <span>{formatLastPrayed()}</span>
+                    </div>
+                    <div>
+                        <Emoji symbol="🎂" />
+                        <span>{formatBirthday()}</span>
+                    </div>
+                </section>
+            </header>
             <pre>{JSON.stringify(data, null, 2)}</pre>
             <button onClick={handleDelete}>Delete</button>
         </div>
