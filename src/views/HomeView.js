09e74ad18e95f61810f815a88afbbd37c7b576f7
Sean McPherson <git@seanmcp.com>
2019-09-03 06:48:59 -0400

    Use view container

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -2,14 +2,18 @@ import React from 'react'
 import Recommendations from 'Recommendations'
 import { Link } from '@reach/router'
 import ROUTES from 'constants/routes'
+import ViewContainer from 'ViewContainer'
+import { SearchInput } from 'evergreen-ui'
 
 function HomeView() {
     return (
-        <div className="HomeView">
-            <Link to={ROUTES.search}>Search</Link>
+        <ViewContainer>
+            <Link to={ROUTES.search}>
+                <SearchInput placeholder="Search names or tags" width="100%" />
+            </Link>
             <Recommendations />
             <Link to={ROUTES.all}>View all</Link>
-        </div>
+        </ViewContainer>
     )
 }
 
--- src/views/SearchView.js
+++ src/views/SearchView.js
@@ -1,24 +1,27 @@
 import React from 'react'
 import { useIndividuals } from 'store/useIndividuals'
 import SearchResults from 'SearchResults'
+import { SearchInput } from 'evergreen-ui'
+import ViewContainer from 'ViewContainer'
 
 function SearchView(props) {
     const [data] = useIndividuals()
     const [query, setQuery] = React.useState('')
     return (
-        <div className="SearchView">
+        <ViewContainer title="Search">
             <header>
-                <input
+                <SearchInput
                     type="search"
                     aria-label="Search"
                     onChange={e => setQuery(e.target.value)}
                     placeholder="Search names or tags"
                     value={query}
+                    width="100%"
                     autoFocus
                 />
             </header>
             <SearchResults data={data} query={query} />
-        </div>
+        </ViewContainer>
     )
 }
 
