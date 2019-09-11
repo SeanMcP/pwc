adf984379cb456df9acc172c02aefae1a454dbe7
Sean McPherson <git@seanmcp.com>
2019-09-11 06:45:21 -0400

    Add app button link

--- src/views/AllView.js
+++ src/views/AllView.js
@@ -3,7 +3,7 @@ import IndividualsList from 'IndividualsList'
 import ROUTES from 'constants/routes'
 import { useIndividuals } from 'store/useIndividuals'
 import ViewContainer from 'ViewContainer'
-import AppLink from 'AppLink'
+import AppButtonLink from 'AppButtonLink'
 import MockSearchbar from 'MockSearchbar'
 
 function AllView() {
@@ -12,9 +12,9 @@ function AllView() {
         <ViewContainer title="All">
             <MockSearchbar />
             <IndividualsList individuals={individuals} />
-            <AppLink to={ROUTES.add} iconBefore="add">
+            <AppButtonLink to={ROUTES.add} iconBefore="add">
                 Add
-            </AppLink>
+            </AppButtonLink>
         </ViewContainer>
     )
 }
