1f49a36532645c204bcdd2aea46d3cc728104134
Sean McPherson <git@seanmcp.com>
2019-09-14 09:42:37 -0400

    Migrate app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from 'components/App/App'
 import './styles/index.scss'
-import App from './App'
 // import { register } from './serviceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
