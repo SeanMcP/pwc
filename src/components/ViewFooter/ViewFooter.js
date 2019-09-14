9d2e5d3d55758d5d6111e71dee8e461018309793
Sean McPherson <git@seanmcp.com>
2019-09-14 10:22:14 -0400

    Add footer nav

--- src/components/ViewFooter/ViewFooter.js
+++ src/components/ViewFooter/ViewFooter.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import ViewContent from 'components/ViewContent/ViewContent'
 import './ViewFooter.scss'
+// import AppLink from 'components/AppLink/AppLink'
+import ROUTES from 'constants/routes'
+import NavLink from 'components/NavLink/NavLink'
 
 function ViewFooter() {
     return (
         <footer className="ViewFooter">
-            <ViewContent>
-                <small>
-                    Made by <a href="https://github.com/seanmcp">SeanMcP</a>
-                </small>
+            <ViewContent squish>
+                <nav className="ViewFooter__nav">
+                    <NavLink className="ViewFooter__link" to={ROUTES.home}>
+                        Home
+                    </NavLink>
+                    <NavLink className="ViewFooter__link" to={ROUTES.search}>
+                        Search
+                    </NavLink>
+                    <NavLink className="ViewFooter__link" to={ROUTES.all}>
+                        All
+                    </NavLink>
+                    <NavLink className="ViewFooter__link" to={ROUTES.settings}>
+                        Settings
+                    </NavLink>
+                </nav>
             </ViewContent>
         </footer>
     )
