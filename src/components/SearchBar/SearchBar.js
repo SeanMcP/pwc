3c22b16848ef38c5a967681b979e56a0609b38ec
Sean McPherson <git@seanmcp.com>
2019-09-25 06:14:46 -0400

    Simplify searchbar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,9 +1,6 @@
 import React from 'react'
-import { Match } from '@reach/router'
-import MockSearchbar from 'MockSearchbar'
 import { SearchField } from 'Form'
 import ViewContent from 'components/ViewContent/ViewContent'
-import ROUTES from 'constants/routes'
 
 import './SearchBar.scss'
 
@@ -11,15 +8,7 @@ function SearchBar(props) {
     return (
         <section className="SearchBar">
             <ViewContent>
-                <Match path={ROUTES.search}>
-                    {({ match }) =>
-                        match ? (
-                            <SearchField {...props} />
-                        ) : (
-                            <MockSearchbar {...props} />
-                        )
-                    }
-                </Match>
+                <SearchField {...props} />
             </ViewContent>
         </section>
     )
