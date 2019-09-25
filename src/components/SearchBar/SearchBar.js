44eb183d2a9d3d159220783feb1ebdf1063862ce
Sean McPherson <git@seanmcp.com>
2019-09-25 06:18:02 -0400

    Restyle SearchBar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import { SearchField } from 'Form'
-import ViewContent from 'components/ViewContent/ViewContent'
 
 import './SearchBar.scss'
 
 function SearchBar(props) {
     return (
         <section className="SearchBar">
-            <ViewContent>
-                <SearchField {...props} />
-            </ViewContent>
+            <SearchField {...props} />
         </section>
     )
 }
