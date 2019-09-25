ad7978c33be5a6b6e790cb45759216757ad71057
Sean McPherson <git@seanmcp.com>
2019-09-25 06:19:40 -0400

    Add search

--- src/views/AllView.js
+++ src/views/AllView.js
@@ -2,14 +2,17 @@ import React from 'react'
 import { useIndividuals } from 'store/useIndividuals'
 import ButtonLink from 'components/ButtonLink/ButtonLink'
 import IndividualsList from 'components/IndividualsList/IndividualsList'
+import SearchBar from 'components/SearchBar/SearchBar'
 import ViewContainer from 'components/ViewContainer/ViewContainer'
 import ROUTES from 'constants/routes'
 
 function AllView() {
+    const [query, setQuery] = React.useState('')
     const [individuals] = useIndividuals()
     return (
         <ViewContainer title="All">
-            <IndividualsList individuals={individuals} />
+            <SearchBar value={query} onChange={e => setQuery(e.target.value)} />
+            <IndividualsList individuals={individuals} query={query} />
             <ButtonLink to={ROUTES.add}>Add</ButtonLink>
         </ViewContainer>
     )
