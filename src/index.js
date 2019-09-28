f8d14980f8b901cb7e72fba335769a6357572124
Sean McPherson <git@seanmcp.com>
2019-09-28 19:49:37 -0400

    Prepare alpha release

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from 'components/App/App'
 import './styles/index.scss'
-// import { register } from './serviceWorker'
+import { register } from './serviceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
-// register()
+register()
