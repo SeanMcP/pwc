0c029be34e45ad76dac1bed4ae7342b8ca67f045
Sean McPherson <git@seanmcp.com>
2019-10-25 07:04:26 -0400

    Create icon component

---
+++ src/components/Icon/Icon.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import * as FeatherIcons from 'react-feather'
+import classList from '@seanmcp/class-list'
+import './Icon.scss'
+
+function Icon({ block = false, className = '', icon, ...props }) {
+    if (!FeatherIcons[icon]) return null
+
+    const Tag = FeatherIcons[icon]
+
+    return (
+        <span className={classList('Icon', block && 'Icon--block', className)}>
+            <Tag
+                className="Icon__svg"
+                {...props}
+                aria-hidden="true"
+                role="img"
+            />
+        </span>
+    )
+}
+
+Icon.defaultProps = {
+    size: 24
+}
+
+export default Icon
