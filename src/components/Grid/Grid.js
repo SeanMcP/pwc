158a82659d11bca2783ff300d17021d75208576d
Sean McPherson <git@seanmcp.com>
2019-10-31 06:26:34 -0400

    Add inline, units props to Grid

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -3,14 +3,22 @@ import classList from '@seanmcp/class-list'
 
 import './Grid.scss'
 
-function Grid({ as = 'div', className, columns, gap, ...props }) {
+function Grid({
+    as = 'div',
+    className,
+    columns,
+    gap,
+    inline = false,
+    units = '1fr',
+    ...props
+}) {
     const Tag = as
     const style = {}
-    if (columns) style.gridTemplateColumns = `repeat(${columns}, 1fr)`
+    if (columns) style.gridTemplateColumns = `repeat(${columns}, ${units})`
     if (gap) style.gridGap = gap
     return (
         <Tag
-            className={classList('Grid', className)}
+            className={classList('Grid', className, inline && 'Grid--inline')}
             style={style}
             {...props}
         />
