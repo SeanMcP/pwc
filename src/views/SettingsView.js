f651d1f5027984e689b862b98d1de94d17a749f1
Sean McPherson <git@seanmcp.com>
2019-11-12 06:20:05 -0500

    Use formik
    Fixes #58

--- src/views/SettingsView.js
+++ src/views/SettingsView.js
@@ -1,35 +1,55 @@
 import React from 'react'
+import { Formik } from 'formik'
 import ViewContainer from 'components/ViewContainer/ViewContainer'
 import useSettings from 'store/useSettings'
 import { Form, InputField } from 'components/Form/Form'
 import Button from 'components/Button/Button'
+import { FIELDS, defaultValues, validationSchema } from 'schemas/settings'
+import Grid from 'components/Grid/Grid'
 
-function SettingsView(props) {
+function SettingsView() {
     const [settings, { setAll }] = useSettings()
-    function handleSubmit(e) {
-        e.preventDefault()
-        const formData = new FormData(e.target)
-        const recommendationCount = formData.get('recommendationCount')
-        if (recommendationCount) {
-            setAll({ recommendationCount })
-        }
+
+    function onSubmit(values) {
+        const { recommendationCount } = values
+        setAll({ recommendationCount })
     }
+
     return (
         <ViewContainer title="Settings">
-            <Form onSubmit={handleSubmit}>
-                <InputField
-                    description="Default is three"
-                    label="Number of recommendations"
-                    name="recommendationCount"
-                    type="number"
-                    defaultValue={settings.recommendationCount}
-                    required
-                />
-                <footer>
-                    <Button modifier={['primary']}>Save</Button>
-                    <Button type="reset">Reset</Button>
-                </footer>
-            </Form>
+            <Formik
+                initialValues={defaultValues({
+                    [FIELDS.recommendationCount]: settings.recommendationCount
+                })}
+                onSubmit={onSubmit}
+                validationSchema={validationSchema}
+            >
+                {({ dirty, handleSubmit }) => (
+                    <Form onSubmit={handleSubmit}>
+                        <InputField
+                            description="Default is three"
+                            label="Number of recommendations"
+                            name="recommendationCount"
+                            type="number"
+                        />
+                        <footer>
+                            <Grid
+                                columns="2"
+                                gap="1rem"
+                                inline
+                                units="min-content"
+                            >
+                                <Button disabled={!dirty} primary type="submit">
+                                    Save
+                                </Button>
+                                <Button disabled={!dirty} type="reset">
+                                    Reset
+                                </Button>
+                            </Grid>
+                        </footer>
+                    </Form>
+                )}
+            </Formik>
         </ViewContainer>
     )
 }
