d6e309905b4d054967f64cf75152e586cd2f062e
Sean McPherson <git@seanmcp.com>
2019-11-19 06:08:02 -0500

    Add icons constant
    Changes prayer icon from 'ArrowUp' to 'Check'

---
+++ src/constants/icons.js
@@ -0,0 +1,20 @@
+const ICONS = {
+    add: 'Plus',
+    favorite: 'Star',
+    home: 'Home',
+    idea: 'Heart',
+    lastPrayed: 'Clock',
+    list: 'List',
+    notes: 'FileText',
+    onboard: 'Smile',
+    person: 'User',
+    place: 'MapPin',
+    prayer: 'Check',
+    search: 'Search',
+    settings: 'Settings',
+    specialDate: 'Calendar',
+    thing: 'Briefcase',
+    verse: 'BookOpen',
+}
+
+export default ICONS
