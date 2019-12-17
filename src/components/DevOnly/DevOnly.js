80d5663ad9e8bdb3ed945edb57d61eb37fd25063
Sean McPherson <git@seanmcp.com>
2019-12-17 07:03:52 -0500

    Fix render logic 🤦‍♂️

--- src/components/DevOnly/DevOnly.js
+++ src/components/DevOnly/DevOnly.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function DevOnly({ children, off = false }) {
     const isOn = !off
-    if (process.env.NODE_ENV === 'development' && !isOn) {
+    if (process.env.NODE_ENV === 'development' && isOn) {
         return <>{children}</>
     }
     return null
