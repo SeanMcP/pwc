bf2905f3dc9c4fa015e9140ea77b287ad07f221e
Sean McPherson <git@seanmcp.com>
2020-01-09 20:07:36 -0500

    Get login form setup

--- src/views/LoginView.jsx
+++ src/views/LoginView.jsx
@@ -2,26 +2,57 @@ import React from 'react'
 import { Formik } from 'formik'
 import { InputField, Form } from 'components/Form/Form'
 import AccountViewContainer from 'components/AccountViewContainer/AccountViewContainer'
+import Grid from 'components/Grid/Grid'
+import Button from 'components/Button/Button'
+import AppLink from 'components/AppLink/AppLink'
+import ROUTES from 'constants/routes'
+import { FIELDS, initialValues, loginValidationSchema } from 'schemas/account'
+
+function handleSubmit(values) {
+    console.log('Values:', values)
+}
 
 function LoginView() {
     return (
         <AccountViewContainer title="Login">
-            <Formik>
-                {({ handleSubmit }) => (
+            <Formik
+                initialValues={initialValues}
+                onSubmit={handleSubmit}
+                validationSchema={loginValidationSchema}
+            >
+                {({ dirty, errors, handleSubmit }) => (
                     <Form onSubmit={handleSubmit}>
                         <InputField
                             label="Email address"
-                            name="email"
+                            name={FIELDS.email}
                             type="email"
                         />
                         <InputField
                             label="Password"
-                            name="password"
+                            name={FIELDS.password}
                             type="password"
                         />
+                        <Grid columns={2} gap="1rem">
+                            <Button
+                                disabled={!dirty || Object.keys(errors).length}
+                                primary
+                            >
+                                Login
+                            </Button>
+                            <Button
+                                disabled={!dirty || Object.keys(errors).length}
+                                type="reset"
+                            >
+                                Cancel
+                            </Button>
+                        </Grid>
                     </Form>
                 )}
             </Formik>
+            <p className="--text-center" style={{ marginTop: '2rem' }}>
+                Don't have an account?{' '}
+                <AppLink to={ROUTES.signUp}>Sign up!</AppLink>
+            </p>
         </AccountViewContainer>
     )
 }
