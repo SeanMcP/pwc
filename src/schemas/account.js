2bcd12343f427c355dc9dfba9dad18979a59a9fe
Sean McPherson <git@seanmcp.com>
2020-01-09 20:07:10 -0500

    Add route schema

---
+++ src/schemas/account.js
@@ -0,0 +1,28 @@
+import * as yup from 'yup'
+
+export const FIELDS = {
+    email: 'email',
+    password: 'password',
+    confirm: 'confirm',
+}
+
+export const initialValues = {
+    [FIELDS.email]: '',
+    [FIELDS.password]: '',
+    [FIELDS.confirm]: '',
+}
+
+const loginShape = {
+    [FIELDS.email]: yup
+        .string()
+        .email('Must be a valid email address')
+        .required('Required'),
+    [FIELDS.password]: yup.string().required('Required'),
+}
+
+export const loginValidationSchema = yup.object().shape(loginShape)
+
+export const signUpValidationSchema = yup.object().shape({
+    ...loginShape,
+    [FIELDS.confirm]: yup.string().required('Required'),
+})
