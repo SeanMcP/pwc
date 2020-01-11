9849bd025fa06c675b5072c23ac7382bd6eda576
Sean McPherson <git@seanmcp.com>
2020-01-11 07:01:24 -0500

    Add register view

---
+++ src/views/RegisterView.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import { Formik } from 'formik'
+import { InputField, Form } from 'components/Form/Form'
+import AccountViewContainer from 'components/AccountViewContainer/AccountViewContainer'
+import Grid from 'components/Grid/Grid'
+import Button from 'components/Button/Button'
+import AppLink from 'components/AppLink/AppLink'
+import ROUTES from 'constants/routes'
+import {
+    FIELDS,
+    initialValues,
+    registerValidationSchema,
+} from 'schemas/account'
+
+function handleSubmit(values) {
+    console.log('Values:', values)
+}
+
+function RegisterView() {
+    return (
+        <AccountViewContainer title="Register">
+            <Formik
+                initialValues={initialValues}
+                onSubmit={handleSubmit}
+                validationSchema={registerValidationSchema}
+            >
+                {({ dirty, errors, handleSubmit }) => (
+                    <Form onSubmit={handleSubmit}>
+                        <InputField
+                            label="Email address"
+                            name={FIELDS.email}
+                            type="email"
+                        />
+                        <InputField
+                            label="Password"
+                            name={FIELDS.password}
+                            type="password"
+                        />
+                        <InputField
+                            label="Confirm password"
+                            name={FIELDS.confirm}
+                            type="password"
+                        />
+                        <Grid columns={2} gap="1rem">
+                            <Button
+                                disabled={!dirty || Object.keys(errors).length}
+                                primary
+                            >
+                                Register
+                            </Button>
+                            <Button
+                                disabled={!dirty || Object.keys(errors).length}
+                                type="reset"
+                            >
+                                Cancel
+                            </Button>
+                        </Grid>
+                    </Form>
+                )}
+            </Formik>
+            <p className="--text-center" style={{ marginTop: '2rem' }}>
+                Already have an account?{' '}
+                <AppLink to={ROUTES.login}>Login!</AppLink>
+            </p>
+        </AccountViewContainer>
+    )
+}
+
+export default RegisterView
