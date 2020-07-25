01a4d11f83fd4a407c83b0810b97fd423e660756
Sean McPherson <git@seanmcp.com>
2020-07-24 20:37:18 -0400

    task: remove fb

--- src/index.js
+++ src/index.js
@@ -3,13 +3,7 @@ import ReactDOM from 'react-dom'
 import App from 'components/App/App'
 import './styles/index.scss'
 import { register } from './serviceWorker'
-import { FirebaseProvider } from 'firebase/useFirebase'
 
-ReactDOM.render(
-    <FirebaseProvider>
-        <App />
-    </FirebaseProvider>,
-    document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
 
 register()
